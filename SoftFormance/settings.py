92cd96ca206dde8364f37700467acd6775ebd863
Vladyslav Yurchenko <vld.yrchnk@gmail.com>
2021-03-17 11:25:41 +0200

    DRF installation and preparation for the API

--- SoftFormance/settings.py
+++ SoftFormance/settings.py
@@ -10,6 +10,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
+    'api.apps.ApiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -17,6 +18,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'feed.apps.FeedConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
