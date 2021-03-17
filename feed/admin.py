51d4bbd661746510d0f9a8abe2c2b9dee0b6fc07
Vladyslav Yurchenko <vld.yrchnk@gmail.com>
2021-03-17 03:24:06 +0200

    Create models

--- feed/admin.py
+++ feed/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import RegisteredUser, FeedItem
+
+
+@admin.register(RegisteredUser)
+class RegisteredUserAdmin(admin.ModelAdmin):
+    list_display = 'user', 'id',
+
+
+@admin.register(FeedItem)
+class FeedItemAdmin(admin.ModelAdmin):
+    list_display = 'user', 'content',
--- feed/models.py
+++ feed/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Ownable(models.Model):
+    """
+    Абстрактная модель юзера владельца поста
+    """
+    user = models.ForeignKey(
+        to='auth.User',
+        on_delete=models.CASCADE,
+        verbose_name="Author",
+        related_name="%(class)ss",
+    )
+
+    class Meta:
+        abstract = True
+
+
+class RegisteredUser(models.Model):
+    """
+    Зарегестрированный юзер
+    """
+    # Юзер
+    user = models.OneToOneField(
+        to=User,
+        on_delete=models.CASCADE,
+    )
+    # Отслеживаемые юзеры
+    tracking = models.ManyToManyField(
+        to='self',
+        related_name='tracked_by',
+        blank=True,
+        symmetrical=False,
+    )
+
+    def __str__(self):
+        return self.user.username
+
+
+class FeedItem(Ownable):
+    content = models.CharField(
+        "Content",
+        max_length=1000,
+        blank=True,
+        null=True,
+    )
+
+    def __str__(self):
+        return f'{self.user.username}: {self.content[:20]}'
