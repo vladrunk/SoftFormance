1df6548e2056d9bb43350e43ca5fb2b28ea96952
Vladyslav Yurchenko <vld.yrchnk@gmail.com>
2021-03-17 12:20:36 +0200

    set postgresql

--- SoftFormance/settings.py
+++ SoftFormance/settings.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 from os import getenv
+import dj_database_url
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -52,12 +54,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'SoftFormance.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {'default': dj_database_url.config(conn_max_age=600, ssl_require=True)}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
